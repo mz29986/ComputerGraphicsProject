5af3e2e9ac43eff0fbb69c7cd9f28d3afdf6ca1e
mz29986 <mz29986@seeu.edu.mk>
2025-02-11 00:23:31 +0100

    added loading screen while loading 3d assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { App } from "./App";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  </React.StrictMode>
+);
